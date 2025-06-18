978a6af693ab376602dbb81952afd95a75bb2af0
Deepali Verma <137193529+Deepali73@users.noreply.github.com>
2025-06-18 16:09:10 +0530

    Update call.py

--- call.py
+++ call.py
@@ -1,24 +1,3 @@
-----------------------#FIRST-APPROACH---------------------------
-import messagebird
-
-#create instance of messagebird.Client using API key
-client = messagebird.Client('<your-api-key>')
-
-try:
-    msg = client.voice_message_create('+91xxxxxxxxxx', 'Hey you, a little bird told me you wanted a call!', { 'voice' : 'female' })
-    print(msg.__dict__)
-
-except messagebird.client.ErrorException as e:
-    for error in e.errors:
-        print(error)
-
-
-
-
-
-
-
----------------------------#SECOND-APPROACH-----------------------------
 from flask import Flask, request, jsonify
 import messagebird
 
