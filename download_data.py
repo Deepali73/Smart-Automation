baea5540ee6bd07a07da98efd9928197ce36e802
Deepali Verma <137193529+Deepali73@users.noreply.github.com>
2025-06-18 16:09:47 +0530

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,31 +1,3 @@
-----------------------------#FIRST-APPROACH-------------------------
-import requests
-import os
-
-# URL of the file you want to download
-url = "https://www.google.com"
-
-# Get the user's Downloads directory
-downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
-file_name = os.path.join(downloads_folder, "sample-file.txt")
-
-# Download the file
-response = requests.get(url)
-
-if response.status_code == 200:
-    with open(file_name, "wb") as file:
-        file.write(response.content)
-    print(f"File downloaded successfully to {file_name}")
-else:
-    print("Failed to download file.")
-
-
-
-
-
-
-
----------------------------#SECOND-APPROACH------------------------------
 from flask import Flask, request, jsonify
 import requests
 import os
