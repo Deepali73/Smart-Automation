0003101fae31e39d468b380692f4a1975335966a
Deepali Verma <137193529+Deepali73@users.noreply.github.com>
2025-06-18 16:15:14 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,25 +1,3 @@
-------------------------#FIRST-APPROACH------------------------
-from twilio.rest import Client
-
-def send_sms_twilio(account_sid: str, auth_token: str, from_number: str, to_number: str, message_body: str):
-    try:
-        client = Client(account_sid, auth_token)
-        message = client.messages.create(
-            body=message_body,
-            from_=from_number,
-            to=to_number
-        )
-        print(f"Message sent successfully! SID: {message.sid}")
-    except Exception as e:
-        print("Error sending message:", e)
-
-
-
-
-
-
-
--------------------------#SECOND-APPROACH-------------------------------
 from flask import Flask, request, jsonify
 from twilio.rest import Client
 
