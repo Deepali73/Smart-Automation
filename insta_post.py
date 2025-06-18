d2cba34f448e29a07ec39ea5c15a74d9e2f75c7f
Deepali Verma <137193529+Deepali73@users.noreply.github.com>
2025-06-18 16:13:00 +0530

    Update insta_post.py

--- insta_post.py
+++ insta_post.py
@@ -1,22 +1,3 @@
-------------------------#FIRST-APPROACH----------------------------
-from instagrapi import Client
-
-def post_to_instagram(username: str, password: str, image_path: str, caption: str) -> str:
-    try:
-        cl = Client()
-        cl.login(username, password)
-        cl.photo_upload(image_path, caption)
-        return "✅ Posted successfully!"
-    except Exception as e:
-        return f"❌ Error: {e}"
-
-
-
-
-
-
-
----------------------------#SECOND-APPROACH----------------------
 from flask import Flask, request, jsonify
 from instagrapi import Client
 
