08599154a071ea44f895b8b391642d4a22396133
Deepali Verma <137193529+Deepali73@users.noreply.github.com>
2025-06-18 16:11:04 +0530

    Update email.py

--- email.py
+++ email.py
@@ -1,32 +1,3 @@
------------------------#FIRST-APPROACH----------------------
-import smtplib
-
-def send_email(sender_email: str, receiver_email: str, app_password: str, message: str):
-    try:
-        # create SMTP session
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()  # secure the connection
-
-        # login with app password
-        server.login(sender_email, app_password)
-
-        # send the email
-        server.sendmail(sender_email, receiver_email, message)
-        print("Email sent successfully!")
-
-    except Exception as e:
-        print("Error:", e)
-
-    finally:
-        server.quit()
-
-
-
-
-
-
-
-------------------------#SECOND-APPROACH-------------------------------
 from flask import Flask, request, jsonify
 import smtplib
 
